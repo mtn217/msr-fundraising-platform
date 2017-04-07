767add780f48cb16caca3dd7acc7095e279259fe
Michael Nguyen <mtn217@uw.edu>
2017-04-07 00:22:08 -0700

    Completed form on front end and backend, will clean up later

--- msrdonations/msrdonation.php
+++ msrdonations/msrdonation.php
@@ -5,16 +5,31 @@
    Version: 0.1
    Author: Michael Nguyen
    */
+   if(!defined('STRIPE_BASE_URL')) {
+   	define('STRIPE_BASE_URL', plugin_dir_url(__FILE__));
+   }
+   if(!defined('STRIPE_BASE_DIR')) {
+   	define('STRIPE_BASE_DIR', dirname(__FILE__));
+   }
 
  function donation_admin() {
      include('donation_admin.php');
  }
 
-function donation_admin_actions() {
-    add_menu_page("MSR Donation Platform", "MSR Donation Platform", 1, "MSR Donation Platform", "donation_admin", "dashicons-chart-line", 79);
-}
+// function donation_admin_actions() {
+//     add_menu_page("MSR Donation Platform", "MSR Donation Platform", 1, "MSR Donation Platform", "donation_admin", "dashicons-chart-line", 79);
+// }
 
-add_action('admin_menu', 'donation_admin_actions');
+// add_action('admin_menu', 'donation_admin_actions');
+
+
+if(is_admin()) {
+  include(STRIPE_BASE_DIR . '\includes\settings.php');
+} else {
+  include(STRIPE_BASE_DIR . '\includes\shortcodes.php');
+  include(STRIPE_BASE_DIR . '\includes\scripts.php');
+  include(STRIPE_BASE_DIR . '\includes\process-payment.php');
+}
 
 /*
 https://code.tutsplus.com/tutorials/create-a-custom-wordpress-plugin-from-scratch--net-2668
